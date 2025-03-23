3c6293359e7ee9916792d417d709cccf76332c38
DongWook Lee <sh95119@gmail.com>
2025-03-23 09:58:49 +0900

    소켓 욥션 변경

--- config/runtime.exs
+++ config/runtime.exs
@@ -34,7 +34,7 @@ if config_env() == :prod do
     # ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: :inet6,
+    socket_options: [:inet],
     show_sensitive_data_on_connection_error: true
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
