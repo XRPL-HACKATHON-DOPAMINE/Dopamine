fbf3f07662486f7ccb053f7343855d576f0761eb
Lee Dong Wook <sh95119@gmail.com>
2025-05-01 18:36:55 +0900

    FEAT: 라이브 디버거랑 tidewave 추가

--- mix.exs
+++ mix.exs
@@ -59,7 +59,8 @@ defmodule Dopamin.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.5"},
-      {:live_debugger, "~> 0.1.2", only: :dev}
+      {:live_debugger, "~> 0.1.2", only: :dev},
+      {:tidewave, "~> 0.1", only: :dev}
     ]
   end
 
